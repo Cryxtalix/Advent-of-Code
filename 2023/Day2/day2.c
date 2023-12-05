f6b660a741e93971299e2550c68ec8b5b7b4702e
Cryxtalix <61549886+Cryxtalix@users.noreply.github.com>
2023-12-05 17:37:07 +0800

    Added day2-2

--- 2023/Day2/day2.c
+++ 2023/Day2/day2.c
@@ -1,6 +1,53 @@
 #include "../lib/input.h"
 #include <stdbool.h>
 
+void part2(text_file *input)
+{
+        int total_power = 0;
+
+        // For each line
+        for (int i = 0; i < input->sentence_num; i++) {
+
+                int colors[3] = {0, 0, 0}; // Red, Green, Blue
+
+                int tmp = 0;
+
+                // Tokenise each line with strtok loop
+                for (char *token = strtok(input->text_body[i], " "), tok_idx = 0; token != NULL; token = strtok(NULL, " "), tok_idx++) {
+
+                        if (tok_idx > 1) {
+                                // Cube numbers
+                                if (tok_idx % 2 == 0) {
+                                        tmp = atoi(token);
+                                }
+
+                                // Cube colors
+                                if (tok_idx % 2 == 1) {
+                                        if (token[0] == 'r') {
+                                                if (tmp > colors[0]) {
+                                                        colors[0] = tmp;
+                                                }
+                                        }
+                                        else if (token[0] == 'g') {
+                                                if (tmp > colors[1]) {
+                                                        colors[1] = tmp;
+                                                }
+                                        }
+                                        else if (token[0] == 'b') {
+                                                if (tmp > colors[2]) {
+                                                        colors[2] = tmp;
+                                                }
+                                        }
+                                }
+                        }
+                }
+
+                total_power += (colors[0] * colors[1] * colors[2]);
+        }
+
+        printf("The total sum of the powers is %d.\n", total_power);
+}
+
 // Used in part 1
 bool check_possibility(int colors[3])
 {
@@ -78,9 +125,12 @@ void part1(text_file *input)
 int main(void)
 {
         text_file *input = read_input("./input.txt");
-
         part1(input);
-
         free_input(input);
+
+        text_file *new_input = read_input("./input.txt");
+        part2(new_input);
+        free_input(input);
+
         return 0;
 }
